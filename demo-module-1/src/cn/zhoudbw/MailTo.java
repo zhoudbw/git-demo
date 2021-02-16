233d6ca1176c2c61bae5fb09d32c72184af853f9
zhoudbw <973121322@qq.com>
2021-02-16 10:59:33 +0800

    Merge branch 'test'
    # Conflicts:
    #	demo-module-1/src/cn/zhoudbw/TestMain.java

--- demo-module-1/src/cn/zhoudbw/MailTo.java
+++ demo-module-1/src/cn/zhoudbw/MailTo.java
@@ -15,6 +15,7 @@ public class MailTo {
 
     public static void main(String[] args) {
 
+        String[] strings = new String[]{"2500年前，人们飞鸽传书;185年前，莫尔斯发明了电报;52年前，第一封电子邮件发出..."};
         try {
             // 1. 创建一个与自己邮箱的连接
             // Properties装载一些邮件的配置信息
@@ -26,27 +27,23 @@ public class MailTo {
 
             // 3. 根据邮件的组成 ——>
             // (1)发件人
-            message.setFrom(new InternetAddress("973121322@qq.com"));
+            message.setFrom(new InternetAddress("9731232@qq.com"));
             // (2)收件人
             // - 收件人类别：TO收件人，CC抄送人，BCC密送
             // - 收件人账号：user1，user2...
-            message.setRecipients(Message.RecipientType.TO, new InternetAddress[] {
-                    new InternetAddress("508128291@qq.com")
+            message.setRecipients(Message.RecipientType.TO, new InternetAddress[]{
+                    new InternetAddress("508282@qq.com")
             });
             // (3)设置邮件标题
-            message.setSubject("威威哥哥和甜妹妹的缠绵");
-            // (4)设置邮件内容
-            String htmlText = "<font size =\"5\" face=\"arial\" >" +
-                    "2500年前，人们飞鸽传书<br>" +
-                    "185年前，莫尔斯发明了电报<br>" +
-                    "52年前，第一封电子邮件发出<br>" +
-                    "... </font>";
-            message.setContent(htmlText, "text/plain;charset=utf-8");
+            message.setSubject("我的小作文");
+            // (4)设置邮件内容，读取方式设置为网页版
+            String htmlText = "<b style='color: pink; background-color: pink;'>" + strings[0] + "</b>";
+            message.setContent(htmlText, "text/html;charset=utf-8");
 
             // 创建发送对象
             Transport transport = session.getTransport();
             // 设置连接信息
-            transport.connect("smtp.qq.com", 25, "973121322", "shermdwpewsubcji");
+            transport.connect("smtp.qq.com", 25, "9731232", "sherpewsucji");
             // 发送邮件
             transport.sendMessage(message, message.getAllRecipients());
 
@@ -54,8 +51,5 @@ public class MailTo {
         } catch (MessagingException e) {
             e.printStackTrace();
         }
-
     }
-
-
 }
